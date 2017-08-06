01af6504cefb36e76c7cfe68d6af689debf8d74d
Lepoldo Caballero <git@pitakill.net>
2017-08-06 14:43:57 -0500

    Refactoring in progress

--- _i3/LCD.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ACTUAL=`grep '^[0-9].' /sys/class/backlight/intel_backlight/brightness`
-
-MAX=`grep '^[0-9].' /sys/class/backlight/intel_backlight/max_brightness`
-
-echo $ACTUAL*100/$MAX | bc
--- _i3/background.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-horizontal=`ls ~/.i3/backgrounds/1920x1080/* | shuf -n 1`
-vertical=`ls ~/.i3/backgrounds/1080x1920/* | shuf -n 1`
-
- feh --bg-scale $horizontal --bg-scale $vertical
